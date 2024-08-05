1c3eb52b30664b8b299700c0631ebd02d0169083
Muhammed Izzet Saglam <m.izzetsaglam@gmail.com>
2024-08-05 19:36:44 +0300

    update

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,154 @@
+return {
+  {
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v4.x",
+    lazy = true,
+    config = false,
+  },
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = true,
+  },
+
+  -- Autocompletion
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      { "L3MON4D3/LuaSnip" },
+      { "hrsh7th/cmp-buffer" },
+      { "hrsh7th/cmp-path" },
+      -- { "hrsh7th/cmp-cmdline" },
+      { "saadparwaiz1/cmp_luasnip" },
+      { "rafamadriz/friendly-snippets" },
+    },
+    config = function()
+      local cmp = require("cmp")
+      local cmp_format = require('lsp-zero').cmp_format({ details = true })
+
+      cmp.setup({
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+          -- { name = "cmdline" },
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-d>"] = cmp.mapping.scroll_docs(4),
+        }),
+        snippet = {
+          expand = function(args)
+            vim.snippet.expand(args.body)
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        formatting = cmp_format,
+      })
+      -- friendly snippets
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
+  },
+
+  -- LSP
+  {
+    "neovim/nvim-lspconfig",
+    cmd = { "LspInfo", "LspInstall", "LspStart" },
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "williamboman/mason.nvim" },
+      { "williamboman/mason-lspconfig.nvim" },
+      { "WhoIsSethDaniel/mason-tool-installer.nvim" },
+    },
+    config = function()
+      local lsp_zero = require("lsp-zero")
+
+      -- lsp_attach is where you enable features that only work
+      -- if there is a language server active in the file
+      ---@diagnostic disable-next-line: unused-local
+      local lsp_attach = function(client, bufnr)
+        local opts = { buffer = bufnr }
+
+        vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+        vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+        vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+        vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+        vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+        vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+        vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+        vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+        vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+        vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+      end
+
+      lsp_zero.extend_lspconfig({
+        sign_text = true,
+        lsp_attach = lsp_attach,
+        capabilities = require("cmp_nvim_lsp").default_capabilities(),
+      })
+
+      lsp_zero.ui({
+        float_border = "rounded",
+        sign_text = {
+          error = "✘",
+          warn = "▲",
+          hint = "⚑",
+          info = "»",
+        },
+      })
+
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      })
+
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "bashls",
+          "clangd",
+          "pyright",
+        },
+        handlers = {
+          -- this first function is the "default handler"
+          -- it applies to every language server without a "custom handler"
+          function(server_name)
+            require("lspconfig")[server_name].setup({})
+          end,
+        },
+      })
+      require("mason-tool-installer").setup({
+        ensure_installed = {
+          "stylua",
+          "shfmt",
+          "hyprls",
+          "clang-format",
+          "prettier",
+          "black",
+          "debugpy",
+          "flake8",
+          "isort",
+          "mypy",
+          "pylint",
+        },
+        auto_update = true,
+      })
+    end,
+  },
+  {
+    "onsails/lspkind.nvim"
+  }
+}
