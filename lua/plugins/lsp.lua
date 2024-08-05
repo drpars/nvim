10c3ef2ca51638fa063ee367868813f0dd57f0fb
Muhammed Izzet Saglam <m.izzetsaglam@gmail.com>
2024-08-05 23:43:05 +0300

    update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -27,6 +27,7 @@ return {
     config = function()
       local cmp = require("cmp")
       local cmp_format = require('lsp-zero').cmp_format({ details = true })
+      local lspkind = require('lspkind')
 
       cmp.setup({
         sources = {
@@ -51,7 +52,18 @@ return {
           completion = cmp.config.window.bordered(),
           documentation = cmp.config.window.bordered(),
         },
-        formatting = cmp_format,
+        -- formatting = cmp_format,
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = 'symbol_text', -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
+            -- maxwidth = 50, -- prevent the popup from showing more than provided characters
+            -- (e.g 50 will not show more than 50 characters)
+            -- can also be a function to dynamically calculate max width such as
+            -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+            -- ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+            show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+          })
+        }
       })
       -- friendly snippets
       require("luasnip.loaders.from_vscode").lazy_load()
