0240c0f3ce78fb4f8d5789c846b3a0da09fa3e0c
Muhammed Izzet Saglam <m.izzetsaglam@gmail.com>
2025-12-19 22:47:10 +0300

    Güncelleme

--- lua/drpars/plugins/dropbar.lua
+++ lua/drpars/plugins/dropbar.lua
@@ -1,38 +1,49 @@
 return {
 	{
 		"Bekaboo/dropbar.nvim",
+		lazy = false,
 		dependencies = {
 			"nvim-tree/nvim-web-devicons",
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 		},
 		config = function()
 			local dropbar = require("dropbar")
 			local sources = require("dropbar.sources")
+			local utils = require("dropbar.utils")
 
-			-- WinBar şeffaflığını garantiye alalım
 			vim.api.nvim_set_hl(0, "WinBar", { bg = "NONE" })
 			vim.api.nvim_set_hl(0, "WinBarNC", { bg = "NONE" })
+			vim.api.nvim_set_hl(0, "DropBarIconKindFile", { fg = "#7aa2f7", bg = "NONE" })
+			vim.api.nvim_set_hl(0, "DropBarIconKindFolder", { fg = "#7aa2f7", bg = "NONE" })
 
 			dropbar.setup({
 				bar = {
 					sources = function(_, _)
-						local items = {
-							-- 'path' kaynağını buraya eklemediğimiz için klasör yolu gözükmez
-							sources.markdown, -- Dosya ikonunu ve ismini sağlar
-							sources.lsp, -- Fonksiyon, Class vb. sembolleri sağlar
+						return {
+							sources.markdown,
+							utils.source.fallback({
+								sources.lsp,
+								sources.treesitter,
+							}), -- Fonksiyon/Metot bilgilerini getirir
 						}
-						return items
 					end,
+					padding = { left = 1, right = 1 },
+					update_debounce = 32,
 				},
-				-- Gereksiz boşlukları veya ikonları sadeleştirmek istersen:
 				icons = {
 					ui = {
 						bar = {
-							separator = "  ", -- Ayraç ikonu
-							extends = "…",
+							separator = "  ", -- eklenti varsayılan ayıracı
 						},
 					},
 				},
+				menu = {
+					win_configs = {
+						border = "rounded",
+					},
+				},
 			})
+      vim.keymap.set('n', '<Leader>;', require('dropbar.api').pick, { desc = 'Pick symbols in winbar' })
 		end,
 	},
 }
