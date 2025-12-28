a469a634c09eb7dc399aea6a0a7b7ffc4ed3150a
Muhammed Izzet Saglam <m.izzetsaglam@gmail.com>
2025-12-28 22:08:18 +0300

    noice sadece komut paletinde çalışacak

--- lua/drpars/plugins/noice.lua
+++ lua/drpars/plugins/noice.lua
@@ -1,96 +1,47 @@
 return {
-	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		opts = {
-			views = {
-				cmdline_popup = {
-					position = {
-						row = 5,
-						col = "50%",
-					},
-					size = {
-						width = 60,
-						height = "auto",
-					},
-					border = {
-						style = "rounded",
-					},
-				},
-				hover = {
-					border = {
-						style = "rounded",
-					},
-					position = { row = 2, col = 2 },
-					win_options = {
-						winhighlight = {
-							Normal = "NormalFloat",
-							FloatBorder = "DiagnosticInfo",
-						},
-					},
-				},
-				popupmenu = {
-					relative = "editor",
-					position = {
-						row = 8,
-						col = "50%",
-					},
-					size = {
-						width = 60,
-						height = 10,
-					},
-					border = {
-						style = "rounded",
-						padding = { 0, 1 },
-					},
-					win_options = {
-						winhighlight = {
-							Normal = "NormalFloat",
-							FloatBorder = "FloatBorder",
-						},
-						winblend = 0,
-					},
-				},
-			},
-			routes = {
-				{
-					filter = {
-						event = "notify",
-						find = "No information available",
-					},
-					opts = { skip = true },
-				},
-			},
-			lsp = {
-				signature = {
-					enabled = true,
-				},
-				hover = {
-					enabled = true,
-				},
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-					["cmp.entry.get_documentation"] = true,
-					['vim.lsp.handlers["textDocument/hover"]'] = true,
-					['vim.lsp.handlers["textDocument/signatureHelp"]'] = true,
-				},
-			},
-			presets = {
-				bottom_search = false,
-				command_palette = true,
-				long_message_to_split = true,
-				inc_rename = false,
-				lsp_doc_border = true,
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	dependencies = { "MunifTanjim/nui.nvim" },
+	opts = {
+		cmdline = { enabled = true, view = "cmdline_popup" },
+		popupmenu = { enabled = true, backend = "nui" },
+		messages = { enabled = true },
+		notify = { enabled = true },
+		lsp = {
+			progress = { enabled = false },
+			hover = { enabled = false },
+			signature = { enabled = false },
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+				["vim.lsp.util.set_highlight"] = false,
+				["vim.lsp.util.open_floating_preview"] = false,
 			},
 		},
-		dependencies = {
-			{ "MunifTanjim/nui.nvim" },
-			{
-				"rcarriga/nvim-notify",
-				opts = {
-					render = "wrapped-compact",
-					stages = "static",
+		views = {
+			cmdline_popup = {
+				position = { row = 5, col = "50%" },
+				size = { width = 60, height = "auto" },
+				border = {
+					style = "rounded",
+					padding = { 0, 0 }, -- Üst/Alt ve Sağ/Sol dolguyu tamamen sıfırladık
+				},
+				win_options = {
+					winblend = 0,
+					-- Çerçeve rengini resimdeki gibi turkuaz/mavi yapmak için DiagnosticInfo kullandık
+					winhighlight = "Normal:NormalFloat,FloatBorder:DiagnosticInfo",
+				},
+			},
+			popupmenu = {
+				relative = "editor",
+				position = { row = 8, col = "50%" },
+				size = { width = 60, height = 10 },
+				border = {
+					style = "rounded",
+					padding = { 0, 0 }, -- Öneri listesindeki boşluğu da sıfırladık
+				},
+				win_options = {
+					winblend = 0,
+					winhighlight = "Normal:NormalFloat,FloatBorder:DiagnosticInfo",
 				},
 			},
 		},
